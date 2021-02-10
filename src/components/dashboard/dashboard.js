1dfb9da2daceb6949d2d03ed40feea04afa979b0
Nicholas Chew <nic.cheww@gmail.com>
2021-02-10 21:24:23 +0800

    wedding invitation

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -2,20 +2,6 @@ import React from "react";
 import style from "./dashboard.module.css";
 
 function Dashboard() {
-  return (
-    <div className={style.mainBody}>
-      <span>Welcome</span>
-      <span>to the</span>
-      <span>Wedding Inviatation</span>
-      <span>of</span>
-      <span>Name</span>
-      <span>&</span>
-      <span>Name</span>
-      <div className={style.buttonContainer}>
-        <div>Details</div>
-        <div>Checkin</div>
-      </div>
-    </div>
-  );
+  return <div className={style.mainBody}></div>;
 }
 export default Dashboard;
