c0535d5fdd631d6aee922523924caaa46a46992c
Nicholas Chew <nic.cheww@gmail.com>
2021-02-11 23:11:06 +0800

    height

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,12 +1,7 @@
 import React, { useEffect } from "react";
 import styles from "./dashboard.module.css";
-import { use100vh } from "react-div-100vh";
 
 function Dashboard() {
-  const height = use100vh() - 61;
-  console.log(height);
-  return (
-    <div style={{ height: `${height}px` }} className={styles.mainBody}></div>
-  );
+  return <div className={styles.mainBody}></div>;
 }
 export default Dashboard;
