ecee9d1b76be39c9a7280bf0f1124450496fdc32
Nicholas Chew <nic.cheww@gmail.com>
2021-02-11 22:27:21 +0800

    update dashboard height

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,19 +1,15 @@
 import React, { useEffect } from "react";
-import style from "./dashboard.module.css";
-
-const appHeight = () => {
-  const doc = document.documentElement;
-  doc.style.setProperty("--app-height", `${window.innerHeight}px`);
-};
+import styles from "./dashboard.module.css";
+import useWindowDimensions from "../../hooks/useWindowDimensions";
 
 function Dashboard() {
-  useEffect(() => {
-    window.addEventListener("resize", appHeight);
-    appHeight();
-    return () => {
-      window.removeEventListener("resize", appHeight);
-    };
-  }, []);
-  return <div className={style.mainBody}></div>;
+  const { height } = useWindowDimensions();
+
+  return (
+    <div
+      style={{ height: `calc(${height}px - 61px)` }}
+      className={styles.mainBody}
+    ></div>
+  );
 }
 export default Dashboard;
