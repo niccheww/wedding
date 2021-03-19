cedfab756e749d53f1c2a5473191c876e5b8a8f3
Nicholas Chew <nic.cheww@gmail.com>
2021-03-19 19:59:11 +0800

    remove comments

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,16 +1,3 @@
-// import React from "react";
-// import styles from "./dashboard.module.css";
-
-// function Dashboard() {
-//   return (
-//     <div className={styles.mainBody}>
-//       <div className={styles.mainTitle}>Ben & Swen</div>
-//       <div className={styles.mainTitle2}>Invitation</div>
-//     </div>
-//   );
-// }
-// export default Dashboard;
-
 import React from "react";
 import styles from "./dashboard.module.css";
 import MessageInput from "../message-input/message-input";
@@ -18,14 +5,14 @@ import MessageInput from "../message-input/message-input";
 function Dashboard() {
   return (
     <div className={styles.mainBody}>
-      <div className={styles.section1Body} id="section1">
+      <div className={styles.section1Body} id="main">
         <div className={styles.section1Line1}>We're getting married</div>
         <div className={styles.section1Line2}>Ben & Swen</div>
-        <a className={styles.section1ButtonContainer} href="#section2">
+        <a className={styles.section1ButtonContainer} href="#well-wish">
           Send well wishes
         </a>
       </div>
-      <div className={styles.section2Body} id="section2">
+      <div className={styles.section2Body} id="well-wish">
         <MessageInput />
       </div>
     </div>
